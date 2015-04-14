b1096424a791a735d561fe883727d8b2eaf5d6f7
HeroCC <HeroCC@users.noreply.github.com>
2015-04-14 19:54:06 -0400

    Added a 'More' button, and moved Steam link into it

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+      // Hide the "view" div.
+      $("#more-btn").hide();
+      // Watch for clicks on the "slide" link.
+      $('#toggle').click(function() {
+        // When clicked, toggle the "view" div.
+        $('#more-btn').slideToggle(400);
+        return false;
+    });
+  });
