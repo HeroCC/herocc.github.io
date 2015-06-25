0b6ad50424ce71f160a878e11706d2d3c77f5178
HeroCC <HeroCC@users.noreply.github.com>
2015-06-25 12:06:58 -0400

    Make pages load faster by prefetching with SmoothState

--- js/ss-trans.js
+++ js/ss-trans.js
@@ -2,6 +2,8 @@
   'use strict';
   var $body = $('html, body'),
       content = $('#main').smoothState({
+        prefetch: true,
+        pageCacheSize: 4,
         // Runs when a link has been activated
         onStart: {
           duration: 1000, // Duration of our animation
