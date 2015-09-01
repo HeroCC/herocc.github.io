ca605ff581dab18dee6cb3ec411453a98f241406
HeroCC <HeroCC@users.noreply.github.com>
2015-09-01 19:40:44 -0400

    Fixed GratiPay display by making my own JS implementation, and various cleanups

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709894!
+  this.get = function(aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    }
+
+    anHttpRequest.open( "GET", aUrl, true );
+    anHttpRequest.send( null );
+  }
+}
