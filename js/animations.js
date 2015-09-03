f608124871b4b18cd9b7e0eaeecbb1ef929c6355
HeroCC <HeroCC@users.noreply.github.com>
2015-09-03 17:36:25 -0400

    Add basic hover text to Twitter & prep for others

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,17 @@
 $('html').addClass('js');
-$(document).ready(function() {
-      // Hide the "view" div.
-      $("#more-btn").hide();
-      // Watch for clicks on the "slide" link.
-      $('#toggle').click(function() {
-        // When clicked, toggle the "view" div.
-        $('#more-btn').slideToggle(400);
-        return false;
-    });
+
+$(document).ready(function() { // More Button
+  // Hide the "view" div.
+  $("#more-btn").hide();
+  // Watch for clicks on the "slide" link.
+  $('#toggle').click(function() {
+    // When clicked, toggle the "view" div.
+    $('#more-btn').slideToggle(400);
+    return false;
   });
+});
+
+function hoverChanger(element, changeTo) {
+  var toChange = element.getElementsByTagName("span")[0];
+  toChange.innerHTML = changeTo;
+};
