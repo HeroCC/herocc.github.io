9a450b7747698fbf20c4a9041a0bac9744be019a
HeroCC <HeroCC@herocc.com>
2015-09-26 11:15:06 -0400

    Cleanup and moved background images to their new folder

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Async HTTP GET Function
 var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709894!
   this.get = function(aUrl, aCallback) {
     var anHttpRequest = new XMLHttpRequest();
@@ -11,7 +12,8 @@ var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709
   }
 }
 
-
+// Gets random images from list of images and path
+// Used for random background images
 var bkg_images = ["forest.jpg", "mtns-lake-nature.jpg"];
 function getRandomImage(imgAr, path) { // http://www.dyn-web.com/code/basics/random_image/random_img_js.php
   path = path || 'img/bkg/'; // default path here
@@ -23,11 +25,6 @@ function getRandomImage(imgAr, path) { // http://www.dyn-web.com/code/basics/ran
   return fullImg;
 }
 
-var bkgImage=getRandomImage();
-alert(bkgImage);
-
+// If no-js is in tag, replace it with js
 var docElement = document.documentElement, jsClassName = ["js"], classes = docElement.className.replace(/(^|\s)no-js(\s|$)/, "").split(" ").concat(jsClassName);
 docElement.className = classes.join(" ");
-
-//document.querySelector('head').style = 'background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + getRandomImage + ') no-repeat center center fixed;';
-//document.getElementsByTagName("head")[0].appendChild('background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + getRandomImage() + ') no-repeat center center fixed;');
