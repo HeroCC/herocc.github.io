a91bb9f82d6c65831e77d636a655af6cff3b2ed3
HeroCC <HeroCC@herocc.com>
2015-09-26 11:14:42 -0400

    Merged YouTube api and Gratipay api into one file

--- js/apis.js
+++ js/apis.js
@@ -1,7 +1,6 @@
-apiKey='AIzaSyBqCs-fmczYiVknKSvZWS8aE93VnKHDCzI'
-channelId='UCRjypPU642xzjARktLuFpzw'
-var ytURL='https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' + channelId + '&eventType=live&maxResults=1&type=video&key=' + apiKey;
-
+ytKey='AIzaSyBqCs-fmczYiVknKSvZWS8aE93VnKHDCzI'
+ytId='UCRjypPU642xzjARktLuFpzw'
+var ytURL='https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' + ytId + '&eventType=live&maxResults=1&type=video&key=' + ytKey;
 ytGet = new HttpClient();
 ytGet.get(ytURL, function(response) {
   var ytResponse = JSON.parse(response);
@@ -12,5 +11,17 @@ ytGet.get(ytURL, function(response) {
     document.getElementById("ytLink").href = 'https://youtube.com/watch?v=' + (ytResponse.items.id.videoId);
     document.getElementById("ytLink").onmouseout = "hoverChanger(this, 'Streaming!')"
     document.getElementById("ytLink").onmouseover = "hoverChanger(this, '/c/HeroCC live on YouTube!')"
+  }
+});
+
+// Gratipay
+gratUsername='HeroCC'
+var gratUrl='https://gratipay.com/~' + gratUsername + '/public.json';
+gratGet = new HttpClient();
+gratGet.get(gratUrl, function(response) {
+  var gratResponse = JSON.parse(response);
+  var receivingVal = (gratResponse.taking);
+  if ( receivingVal !== '0.00' ) {
+    document.getElementById("gratText").innerHTML = ('$' + receivingVal);
   }
 });
