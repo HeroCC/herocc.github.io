8f683845d5ce2194c8c88a96d2a93ba8cb6b3b16
HeroCC <HeroCC@herocc.com>
2015-09-26 10:25:10 -0400

    Very Broken, history only

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709
     anHttpRequest.send( null );
   }
 }
+
+
+var bkg_images = ["forest.jpg", "mtns-lake-nature.jpg"];
+function getRandomImage(imgAr, path) { // http://www.dyn-web.com/code/basics/random_image/random_img_js.php
+  path = path || 'img/bkg/'; // default path here
+  imgAr = imgAr || bkg_images;
+  var num = Math.floor( Math.random() * imgAr.length );
+  var img = imgAr[ num ];
+  fullImg = path + img;
+  // var imgStr = '<img src="' + path + img + '" alt = "">';
+  return fullImg;
+}
+var bkgImg = getRandomImage();
+
+//document.getElementById("headStyle").background = bkgImg;
+//document.head.style="linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + bkgImg + ") no-repeat center center fixed;";
+//document.getElementsByTagName("head")[0].style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + bkgImg + ") no-repeat center center fixed;";
+//document.getElementsByTagName("body")[0].setAttribute('style', "background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + bkgImg + ") no-repeat center center fixed; background-size: cover;");
+
+var docElement = document.documentElement, jsClassName = ["js"], classes = docElement.className.replace(/(^|\s)no-js(\s|$)/, "").split(" ").concat(jsClassName);
+docElement.className = classes.join(" ");
