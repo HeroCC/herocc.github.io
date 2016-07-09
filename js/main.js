56ef3a8cd2eb6b4d15f23dd7694297b89f4b19bd
HeroCC <HeroCC@herocc.com>
2016-07-09 13:39:01 -0400

    Add another background image

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709
 
 // Gets random images from list of images and path
 // Used for random background images
-var bkg_images = ["forest.jpg", "mtns-lake-nature.jpg", "swamp-dusk.jpg", "yosemite.jpg", "china-natgeo-water-forest.jpg", "otago-nz-mtns.jpg", "italian-mtn.jpg", "vesper-forest.jpg", "sea-cave.jpg", "panther-creek.jpg", "colorado-mtns.jpg", "LakeWanakaTree.jpg", "RainyYosemite.jpg", "GreenlandStoneTowers.jpg"];
+var bkg_images = ["forest.jpg", "mtns-lake-nature.jpg", "swamp-dusk.jpg", "yosemite.jpg", "china-natgeo-water-forest.jpg", "otago-nz-mtns.jpg", "italian-mtn.jpg", "vesper-forest.jpg", "sea-cave.jpg", "panther-creek.jpg", "colorado-mtns.jpg", "LakeWanakaTree.jpg", "RainyYosemite.jpg", "GreenlandStoneTowers.jpg", "SwissAlps.jpg", "glaciernationalpark.jpg"];
 function getRandomImage(imgAr, path) { // http://www.dyn-web.com/code/basics/random_image/random_img_js.php
   path = path || 'img/bkg/'; // default path here
   imgAr = imgAr || bkg_images;
@@ -28,3 +28,12 @@ function getRandomImage(imgAr, path) { // http://www.dyn-web.com/code/basics/ran
 // If no-js is in tag, replace it with js
 var docElement = document.documentElement, jsClassName = ["js"], classes = docElement.className.replace(/(^|\s)no-js(\s|$)/, "").split(" ").concat(jsClassName);
 docElement.className = classes.join(" ");
+
+function handleOutboundLinkClicks(event) {
+  ga('send', 'event', {
+    eventCategory: 'button',
+    eventAction: 'click',
+    eventLabel: event.target.href,
+    transport: 'beacon'
+  });
+}
